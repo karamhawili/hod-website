4d20f41711d1b9cb5f3f0827b173833c6c2721de
Karam Abu Hawili <karam.hawili@gmail.com>
2025-12-20 12:45:31 +0200

    Add project type

---
+++ src/sanity/schemaTypes/project.ts
@@ -0,0 +1,41 @@
+import { defineField, defineType } from "sanity";
+
+export const project = defineType({
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Project Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "coverImage",
+      title: "Cover Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "excerpt",
+      title: "Short Description",
+      type: "text",
+      rows: 3,
+      validation: (rule) => rule.max(200),
+    }),
+  ],
+});
