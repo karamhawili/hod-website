4fb1e17d970ddae2468cdd924a5198e9b4f15df5
Karam Abu Hawili <karam.hawili@gmail.com>
2025-12-27 16:09:55 +0200

    Add types

---
+++ src/types/sanity.ts
@@ -0,0 +1,22 @@
+export interface SanityImage {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+  hotspot?: {
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+  };
+}
+
+export interface Project {
+  _id: string;
+  title: string;
+  slug: { current: string };
+  coverImage: SanityImage;
+  excerpt: string;
+  category: string;
+}
