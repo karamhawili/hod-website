1952aa83bf794f36815fa8486cc805a8a74f949f
Karam Abu Hawili <karam.hawili@gmail.com>
2026-02-05 01:05:09 +0200

    Add AboutHero

---
+++ src/app/about/_components/AboutHero/AboutHero.tsx
@@ -0,0 +1,35 @@
+import Section from "@/components/Section";
+import Image from "next/image";
+import styles from "./AboutHero.module.css";
+
+export default function AboutHero() {
+  return (
+    <Section shadow background="white" className={styles.wrapper}>
+      <div className={styles.logo}>
+        <Image src="/logo.svg" alt="House of Design" width={200} height={88} />
+      </div>
+      <Section.Header>
+        <Section.Title>About Us</Section.Title>
+        <Section.Description>
+          We create spaces inspired by the emotions we’ve experienced, so you
+          can step inside, live the moment, and leave with emotions of your own.
+        </Section.Description>
+      </Section.Header>
+      <div className={styles.image}>
+        <Image
+          src="/sof.jpeg"
+          alt="House of Design"
+          width={1200}
+          height={538}
+        />
+        <div className={styles.overlay}>
+          <p className={styles.quote}>
+            &apos;There is a crack in everything, that&apos;s how the light gets
+            in.&apos;
+          </p>
+          <p className={styles.author}>- Leonard Cohen</p>
+        </div>
+      </div>
+    </Section>
+  );
+}
