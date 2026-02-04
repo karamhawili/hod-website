58c006f614af168732d01cbd8593a03958c40468
Karam Abu Hawili <karam.hawili@gmail.com>
2026-02-05 01:04:57 +0200

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer/Footer";
+import Navigation from "@/components/Navigation/Navigation";
+import AboutHero from "./_components/AboutHero/AboutHero";
+
+export default function About() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <AboutHero />
+      </main>
+      <Footer />
+    </>
+  );
+}
