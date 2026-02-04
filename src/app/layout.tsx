a9bfe69b791fdadce23ff4bf3547b80ccdae107d
Karam Abu Hawili <karam.hawili@gmail.com>
2026-02-05 00:57:57 +0200

    Add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
-import { Cormorant_Garamond, Darker_Grotesque } from "next/font/google";
+import {
+  Cormorant_Garamond,
+  Darker_Grotesque,
+  Alex_Brush,
+} from "next/font/google";
 import "./globals.css";
 
 const cormorant = Cormorant_Garamond({
@@ -13,6 +17,13 @@ const darkerGrotesque = Darker_Grotesque({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-darker-grotesque",
+  display: "swap",
+});
+
+const alexBrush = Alex_Brush({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-alex-brush",
   display: "swap",
 });
 
@@ -30,7 +41,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${cormorant.variable} ${darkerGrotesque.variable}`}
+      className={`${cormorant.variable} ${darkerGrotesque.variable} ${alexBrush.variable}`}
     >
       <body>{children}</body>
     </html>
