4f61e6fa783ecd42b8b23c21aedb14d4daa7d450
Karam Abu Hawili <karam.hawili@gmail.com>
2026-02-09 00:17:25 +0200

    Change animation

--- src/hooks/useScrollAnimation.ts
+++ src/hooks/useScrollAnimation.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 
-export function useScrollAnimation(threshold = 0.3) {
+export function useScrollAnimation(threshold = 0.1) {
   const ref = useRef<HTMLElement>(null);
   const [isVisible, setIsVisible] = useState(false);
 
