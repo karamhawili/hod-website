2b50d71ed4431067a6e1c991b49eea68f0e62b87
Karam Abu Hawili <karam.hawili@gmail.com>
2026-02-12 01:45:57 +0200

    Make max-width option in SectionContent

--- src/components/Section/SectionContent.tsx
+++ src/components/Section/SectionContent.tsx
@@ -3,8 +3,18 @@ import styles from "./Section.module.css";
 
 interface SectionContentProps {
   children: ReactNode;
+  hasMaxWidth?: boolean; // Optional prop to control max width of content
 }
 
-export default function SectionContent({ children }: SectionContentProps) {
-  return <div className={styles.content}>{children}</div>;
+export default function SectionContent({
+  hasMaxWidth = true,
+  children,
+}: SectionContentProps) {
+  return (
+    <div
+      className={`${styles.content} ${hasMaxWidth ? styles.maxWidth : ""}`.trim()}
+    >
+      {children}
+    </div>
+  );
 }
